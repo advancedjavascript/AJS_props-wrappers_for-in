11220050f559a3cdaf3fa06f20b528c9f5b96932
kate_gaw <ekaterinagaw@gmail.com>
2019-12-09 13:14:39 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
 export default function orderByProps(obj, props) {
-    var result = [];
+  const result = [];
 
-    for (const prop in obj) {
-        if (obj.hasOwnProperty.call(obj, prop)) {
-            result.push({
-                key: prop,
-                value: obj[prop],
-            });
-        }
+  for (const prop in obj) {
+    if (obj.hasOwnProperty.call(obj, prop)) {
+      result.push({
+        key: prop,
+        value: obj[prop],
+      });
     }
-    const filterByProps = result.filter(arr => props.includes(arr.key));
-    const filterOthers = result.filter(arr => !props.includes(arr.key));
-    const sortByAbs = filterOthers.sort((a, b) => (a.key < b.key ? -1 : 1));
+  }
+  const filterByProps = result.filter((arr) => props.includes(arr.key));
+  const filterOthers = result.filter((arr) => !props.includes(arr.key));
+  const sortByAbs = filterOthers.sort((a, b) => (a.key < b.key ? -1 : 1));
 
-    return ([...filterByProps, ...sortByAbs]);
+  return ([...filterByProps, ...sortByAbs]);
 }
